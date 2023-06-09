1383b41180e5b7bf02726b1318d5c492064437dd
nitinranjan512 <106030939+nitinranjan512@users.noreply.github.com>
2023-06-09 16:31:50 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "SmithaOrg"
+    organization = "NitinOrg"
 
     workspaces {
       name = "sentinel-example"
